51fad6c37bb5b61c3eedebcf79d6819a7347b6cb
Yani Isaac U. Santos <santosyaniisaac@gmail.com>
2024-05-24 19:46:13 +0800

    Version1

---
+++ app/src/main/java/com/example/quickbitez/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.quickbitez;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.navigation_menu) {
+                    selectedFragment = new MenuFragment(); // Assuming you have this fragment
+                } else if (itemId == R.id.navigation_cart) {
+                    selectedFragment = new CartFragment();
+                } else if (itemId == R.id.navigation_profile) {
+                    selectedFragment = new ProfileFragment();
+                }
+
+                if (selectedFragment != null) {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragment_container, selectedFragment)
+                            .commit();
+                }
+                return true;
+            }
+        });
+
+        // Set default selection
+        navigation.setSelectedItemId(R.id.navigation_menu);
+    }
+}
